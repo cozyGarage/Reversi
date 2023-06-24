e53185f1e81f97732f5d5c743e87196cdb3979ad
cozygarage <e1500953@edu.vamk.fi>
2023-06-24 13:19:20 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 
 // Create a new instance of an Express Router
-var router = express.Router();
+var router = module.exports =  express.Router();
 
 /* GET home page. */
 // Define a route handler for the GET request method on the root path ("/")
@@ -11,5 +11,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// Export the router to be used in other parts of the application
-module.exports = router;
+
