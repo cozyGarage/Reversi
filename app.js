32798b815114cd50c92dcafae6545d9d8a7d6198
cozygarage <e1500953@edu.vamk.fi>
2023-06-24 11:47:51 +0300

    update comment for boilerplate

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
+// Import required modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Import route modules
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Create an Express application instance
 var app = express();
 
-// view engine setup
+// Set up view engine for rendering templates
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware setup
+app.use(logger('dev')); // Log HTTP requests to the console
+app.use(express.json()); // Parse JSON data in the request body
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data in the request body
+app.use(cookieParser()); // Parse cookies from the request
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' directory
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Define routes
+app.use('/', indexRouter); // Use the 'indexRouter' for requests to the root URL ('/')
+app.use('/users', usersRouter); // Use the 'usersRouter' for requests to '/users' URL
 
-// catch 404 and forward to error handler
+// Catch 404 errors and forward to the error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, providing error details in development environment
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+// Import the Express module
 var express = require('express');
+
+// Create a new instance of an Express Router
 var router = express.Router();
 
 /* GET home page. */
+// Define a route handler for the GET request method on the root path ("/")
 router.get('/', function(req, res, next) {
+  // Render the "index" view template and pass in an object with a title property set to "Express"
   res.render('index', { title: 'Express' });
 });
 
+// Export the router to be used in other parts of the application
 module.exports = router;
